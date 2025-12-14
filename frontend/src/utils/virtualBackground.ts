b85dfad56e0e541c3ecaebf8cd8028280648c676
Nguyen Tan Dung <09tandungnguyen@gmail.com>
2025-12-15 06:27:52 +0700

    fix bug when click create room

--- frontend/src/utils/virtualBackground.ts
+++ frontend/src/utils/virtualBackground.ts
@@ -1,4 +1,5 @@
 import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-backend-webgl';
 import * as bodyPix from '@tensorflow-models/body-pix';
 
 let segmentationModel: bodyPix.BodyPix | null = null;
